310dcc8c6ec41a8b4cb8c978e1137005ca6f6e1f
Alexey Sokolov <sokol789@yandex.ru>
2016-05-26 14:39:15 +0300

    first start

---
+++ app.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+import { bootstrap } from "@angular/platform-browser-dynamic";
+
+@Component({
+    selector: 'app',
+    template: '<div>app</div>'
+})
+export class App {}
+
+bootstrap(App);
