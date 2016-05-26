7dc8237eec4534636d21cea12176ae8668d01c71
Alexey Sokolov <sokol789@yandex.ru>
2016-05-26 16:02:45 +0300

    es6 typings

--- app.ts
+++ app.ts
@@ -1,9 +1,11 @@
+/// <reference path="typings/tsd.d.ts" />
+
 import { Component } from "@angular/core";
 import { bootstrap } from "@angular/platform-browser-dynamic";
 
 @Component({
     selector: 'app',
-    template: '<div>app 2</div>'
+    template: '<div>app 3</div>'
 })
 export class App {}
 
