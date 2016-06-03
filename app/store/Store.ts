3e7f299db9230369c4a78f71117eba98a1d43a64
Alexey Sokolov <sokol789@yandex.ru>
2016-06-03 13:02:56 +0300

    store wrapper

--- app/store/Store.ts
+++ app/store/Store.ts
@@ -1,23 +1,28 @@
 import { Injectable } from "@angular/core";
 import { Observable } from 'rxjs';
+import { Unsubscribe } from 'redux';
 import { NgRedux } from 'ng2-redux';
 import { Action } from "./Action";
 import { AppState } from "./AppState";
 
 @Injectable()
 export class Store {
-    constructor(private ngRedux: NgRedux<AppState>) {
+    constructor(private ngRedux:NgRedux<AppState>) {
     }
 
-    dispatch<A extends Action> (action: A) {
+    dispatch<A extends Action> (action:A) {
         return this.ngRedux.dispatch(<any> action);
     }
 
-    select<S>(selector: string | number | symbol | ((state: AppState) => S), comparer?: (x: any, y: any) => boolean): Observable<any> {  //TODO <any> or <S> ?
+    select<S>(selector:string | number | symbol | ((state:AppState) => S), comparer?:(x:any, y:any) => boolean):Observable<any> {  //TODO <any> or <S> ?
         return this.ngRedux.select(selector);
     }
 
-    getState(): AppState {
+    getState():AppState {
         return this.ngRedux.getState();
+    }
+
+    subscribe(listener:() => void): Unsubscribe {
+        return this.ngRedux.subscribe(listener);
     }
 }
