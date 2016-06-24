e4088832cf6cc599caea7506dbd72eeae8c2dc99
Alexey Sokolov <sokol789@yandex.ru>
2016-06-24 15:57:09 +0300

    prod hashes

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,13 +1,23 @@
 var webpackMerge = require('webpack-merge');
 var commonConfig = require('./webpack.common.js');
+var WebpackMd5Hash = require('webpack-md5-hash');
 
 module.exports = webpackMerge(commonConfig, {
 
     devtool: 'source-map',
 
+    output: {
+        filename: '[name].[chunkhash].js',
+        chunkFilename: '[id].[chunkhash].chunk.js'
+    },
+
     htmlLoader: {
         minimize: false // workaround for ng2
-    }
+    },
+
+    plugins: [
+        new WebpackMd5Hash()
+    ]
 
 });
 
