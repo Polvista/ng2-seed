51c3e3222eeef21e648f2a7ac5f80a4189fd20e7
Polvista <sokol789@yandex.ru>
2016-06-28 19:06:53 +0300

    add rucksack

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -24,7 +24,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                exclude: [/app.scss$/],
+                exclude: [/app\.scss$/],
                 loaders: ['to-string-loader', 'css', 'postcss', 'sass']
             },
             {
