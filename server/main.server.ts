c6f71dfac1d067107747e335f5e2c28efeda93d3
Alexey Sokolov <sokol789@yandex.ru>
2016-06-29 14:13:35 +0300

    whatever

---
+++ server/main.server.ts
@@ -0,0 +1,37 @@
+import {
+    REQUEST_URL,
+    ORIGIN_URL,
+    NODE_LOCATION_PROVIDERS,
+    NODE_HTTP_PROVIDERS,
+    ExpressEngineConfig
+} from 'angular2-universal';
+
+import { provideRouter } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+
+// Application
+import {App} from '../app/AppComponent';
+
+export function ngApp(req, res) {
+    let baseUrl = '/';
+    let url = req.originalUrl || '/';
+
+    let config: ExpressEngineConfig = {
+        directives: [
+            App
+        ],
+        platformProviders: [
+            {provide: ORIGIN_URL, useValue: 'http://localhost:3000'},
+            {provide: APP_BASE_HREF, useValue: baseUrl},
+        ],
+        providers: [
+            {provide: REQUEST_URL, useValue: url},
+            NODE_HTTP_PROVIDERS,
+            NODE_LOCATION_PROVIDERS
+        ],
+        async: true,
+        preboot: false // { appRoot: 'app' } // your top level app component selector
+    };
+
+    res.render('index', config);
+}
--- server/server.ts
+++ server/server.ts
@@ -1,13 +1,36 @@
-import { App } from '../app/AppComponent';
+import 'angular2-universal/polyfills';
 
+import * as path from 'path';
 import * as express from 'express';
+import * as bodyParser from 'body-parser';
+
+// Angular 2
+import { enableProdMode } from '@angular/core';
+// Angular 2 Universal
+import { expressEngine } from 'angular2-universal';
+
+// enable prod for faster renders
+enableProdMode();
 
 const app = express();
+const ROOT = path.join(path.resolve(__dirname, '..'));
 
-app.get('/', function (req, res) {
-    res.send('Hello World!');
-});
+// Express View
+app.engine('.html', expressEngine);
+app.set('views', __dirname);
+app.set('view engine', 'html');
 
-app.listen(9999, function () {
-    console.log('Example app listening on port 3000!');
+app.use(bodyParser.json());
+
+// Serve static files
+app.use(express.static(ROOT, {index: false}));
+
+import { ngApp } from './main.server';
+
+// Routes with html5pushstate
+app.use('/', ngApp);
+
+// Server
+app.listen(9999, () => {
+    console.log('Listening on: http://localhost:3000');
 });
